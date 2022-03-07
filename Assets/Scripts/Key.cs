0fb7f7b231d04f0e9efa7195aa4f04cf66d2ee01
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2022-03-08 00:03:02 +0400

    keys added

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Key", menuName = "ScriptableObjects/Key", order = 1)]
+public class Key : ScriptableObject
+{
+    
+}

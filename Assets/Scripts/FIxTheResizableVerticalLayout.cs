c4059f44f67b1406494845388b8e07feba029472
dmakarichev <d.a.makarychev@samsmu.ru>
2022-03-11 17:20:09 +0400

    charlit wip

---
+++ Assets/Scripts/FIxTheResizableVerticalLayout.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FIxTheResizableVerticalLayout : MonoBehaviour
+{
+    public void Fix()
+    {
+        transform.position += Vector3.up;
+    }
+}
